dd6f9998e51d37ef2ed5a55b016cffca2cc6bfdf
Saiyan <shaaz.sid@live.com>
2026-01-29 12:02:56 +0500

    Removed CPRI related dropdown from navigation menus in multiple files.

--- lab-testing.php
+++ lab-testing.php
@@ -971,15 +971,6 @@
                     <li><a href="index.php">Home</a></li>
                     <li><a href="about.php">About</a></li>
                     <li><a href="product.php">Product Catalog</a></li>
-                    <li class="dropdown">
-                        <?php if ($isAdminLoggedIn): ?>
-                            <a href="lab-testing.php">Lab Testing <i class="fas fa-chevron-down"></i></a>
-                            <div class="dropdown-content">
-                                <a href="report.php">Report</a>
-                                <a href="cpri.php">CPRI Testing</a>
-                            </div>
-                        <?php endif; ?>
-                    </li>
                     <li>
                         <?php if ($isAdminLoggedIn): ?>
                             <a href="config/logout.php" style="color: var(--danger-red); font-weight: 700;">Logout</a>
